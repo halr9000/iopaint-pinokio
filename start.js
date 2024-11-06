5405cd811aec1368b329fb8a4ddd9ebc4433f1b2
6Morpheus6 <wanton-decay@gmx.net>
2024-11-06 08:31:57 +0100

    normal mode

--- start.js
+++ start.js
@@ -8,7 +8,7 @@ module.exports = {
         env: { },                   // Edit this to customize environment variables (see documentation)
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "iopaint start --config iopaint-config.json",
+          "iopaint start",
         ],
         on: [{
           // The regular expression pattern to monitor.
