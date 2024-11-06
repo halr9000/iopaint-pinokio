9b7908508efd18c88b3d5920e39156eec3f3d030
6Morpheus6 <wanton-decay@gmx.net>
2024-11-06 08:38:30 +0100

    remove flag

--- web-config.js
+++ web-config.js
@@ -8,7 +8,7 @@ module.exports = {
         env: { },                   // Edit this to customize environment variables (see documentation)
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "iopaint start-web-config --config-file iopaint-config.json",
+          "iopaint start-web-config",
         ],
         on: [{
           // The regular expression pattern to monitor.
