ac9d32a000ece498b2cbd6b8e4bf48527eff1210
6Morpheus6 <wanton-decay@gmx.net>
2024-11-12 21:37:14 +0100

    kernel

--- pinokio.js
+++ pinokio.js
@@ -4,7 +4,7 @@ module.exports = {
   title: "IOPaint",
   description: "Image inpainting tool powered by SOTA AI models. Remove any unwanted object, defect, or even people from your pictures, and replace (powered by stable diffusion) anything in your pictures. https://www.iopaint.com/",
   icon: "icon.jpg",
-  menu: async (info) => {
+  menu: async (kernel, info) => {
     let installing = info.running("install.js")
     let installed = info.exists("app/env")
     let running = info.running("start.js")
@@ -44,54 +44,54 @@ module.exports = {
           }]
         }
       } else if (running) {
-          let local = info.local("start.js")
-          if (local && local.url) {
-            return [{
-              default: true,
-              icon: "fa-solid fa-rocket",
-              text: "Open Web UI",
-              href: local.url,
-            }, {
-              icon: 'fa-solid fa-terminal',
-              text: "Terminal",
-              href: "start.js",
-            }, {
-              icon: "fa-solid fa-plug",
-              text: "IOPaint Settings",
-              href: "web-config.js",
-            }]
-          } else {
-            return [{
-              default: true,
-              icon: 'fa-solid fa-terminal',
-              text: "Terminal",
-              href: "start.js",
-            }]  
-          }
-    } else if (config) {
-      return [{
-        default: true,
-        icon: "fa-solid fa-power-off",
-        text: "Start",
-        href: "start-config.js",
-      }, {
-        icon: "fa-solid fa-plug",
-        text: "IOPaint Settings",
-        href: "web-config.js",
-      }, {
-        icon: "fa-solid fa-plug",
-        text: "Update",
-        href: "update.js",
-      }, {
-        icon: "fa-solid fa-plug",
-        text: "Install",
-        href: "install.js",
-      }, {
-        icon: "fa-regular fa-circle-xmark",
-        text: "Reset",
-        href: "reset.js",
-      }]
-    } else {
+        let local = info.local("start.js")
+        if (local && local.url) {
+          return [{
+            default: true,
+            icon: "fa-solid fa-rocket",
+            text: "Open Web UI",
+            href: local.url,
+          }, {
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.js",
+          }, {
+            icon: "fa-solid fa-plug",
+            text: "IOPaint Settings",
+            href: "web-config.js",
+          }]
+        } else {
+          return [{
+            default: true,
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.js",
+          }]  
+        }
+      } else if (config) {
+        return [{
+          default: true,
+          icon: "fa-solid fa-power-off",
+          text: "Start",
+          href: "start-config.js",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "IOPaint Settings",
+          href: "web-config.js",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Update",
+          href: "update.js",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Install",
+          href: "install.js",
+        }, {
+          icon: "fa-regular fa-circle-xmark",
+          text: "Reset",
+          href: "reset.js",
+        }]
+      } else {
         return [{
           default: true,
           icon: "fa-solid fa-power-off",
